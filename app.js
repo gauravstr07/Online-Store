bbe069bd77f89952a7e534c8cc1e914fea1699e4
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-11-04 22:28:18 +0530

    Creating & Fetching Cart

--- app.js
+++ app.js
@@ -46,8 +46,8 @@ Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  .sync({ force: true })
-  //  .sync()
+  //.sync({ force: true })
+  .sync()
   .then((result) => {
     return User.findByPk(1);
   })
@@ -62,6 +62,9 @@ sequelize
   })
   .then((user) => {
     // console.log(user);
+    return user.createCart();
+  })
+  .then(() => {
     app.listen(port, () => {
       console.log(`server running on port: ${port}📡`);
     });
