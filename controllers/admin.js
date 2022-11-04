1fd88481632dc0a6b8b022740b96ed01ab6321f5
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-11-04 16:25:36 +0530

    Getting single product with single product

--- controllers/admin.js
+++ controllers/admin.js
@@ -21,7 +21,7 @@ exports.postAddProduct = (req, res, next) => {
     description: description,
   })
     .then((result) => {
-      console.log(result);
+      console.log("Product Created..");
     })
     .catch((err) => {
       console.log(err);
