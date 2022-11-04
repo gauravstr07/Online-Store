59887d53897a455fa2317103b3b90d44dba13fea
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-11-04 15:37:54 +0530

    Defining Sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize("node-complete", "root", "Akshu2680", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "Akshu2680",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
